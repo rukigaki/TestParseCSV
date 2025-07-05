bfb30149d7e8be358724253233d6bd2e071d2502
rukigaki <rukigaki@gmail.com>
2025-07-05 16:00:48 +0300

    Добавил help для argparse

--- src/engine_parse.py
+++ src/engine_parse.py
@@ -3,13 +3,11 @@ import argparse
 
 class EngineParse:
     def __init__(self):
-        # TODO: Можно ли установить порядок передачи аргументов, и можно ли привязать один аргумент к другому,
-        #  чтобы один был зависим от другого при вызове???
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("--file") # TODO: Дописать строку для --help
-        self.parser.add_argument("--where") # TODO: Дописать строку для --help
-        self.parser.add_argument("--aggregate") # TODO: Дописать строку для --help
-        self.parser.add_argument("--order_by") # TODO: Дописать строку для --help
+        self.parser.add_argument("--file", help="Путь к CSV-файлу для обработки.")
+        self.parser.add_argument("--where", help="Фильтрация строк по условию.")
+        self.parser.add_argument("--aggregate", help="Агрегация по столбцу. Пример: price=avg (поддерживаются: avg, min, max).")
+        self.parser.add_argument("--order_by", help="Сортировка по столбцу. Пример: price:asc или rating:desc.")
 
 
     @property
